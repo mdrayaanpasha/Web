4cbd50f346235059bc1c64dd069882c2373c5d3d
mdrayaanpasha <mdrayaanpasha@gmail.com>
2024-11-26 19:55:04 +0530

    chekcin

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-    <h1 className='text-3xl font-bold underline'> Hello world! </h1> 
+    <h1 className='text-3xl font-bold underline'> Hello world, better then house </h1> 
     );
 };
 
